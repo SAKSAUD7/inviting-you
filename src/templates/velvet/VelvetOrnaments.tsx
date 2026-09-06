7503bbd3798804a220fa5026eb8971fcc84e81c9
SAKSAUD7 <saksaud7411@gmail.com>
2026-09-07 02:04:45 +0530

    fix VelvetFloralDivider background to ivory

--- src/templates/velvet/VelvetOrnaments.tsx
+++ src/templates/velvet/VelvetOrnaments.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const VelvetFloralDivider = () => (
-  <div className="floral-divider" aria-hidden="true" style={{ width: '100%', display: 'flex', justifyContent: 'center', margin: '4rem 0', pointerEvents: 'none' }}>
+  <div className="floral-divider" aria-hidden="true" style={{ width: '100%', display: 'flex', justifyContent: 'center', padding: '4rem 0', background: 'var(--ivory)', pointerEvents: 'none' }}>
     {/* eslint-disable-next-line @next/next/no-img-element */}
     <img src="/assets/images/floral-divider.webp" alt="" style={{ maxWidth: '300px', width: '80%', filter: 'brightness(0.9) contrast(1.2)' }} />
   </div>
