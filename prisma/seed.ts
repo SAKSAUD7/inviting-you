780472fa32fffa2ca518b2a8e8455b5e62a737a8
SAKSAUD7 <saksaud7411@gmail.com>
2026-09-07 02:51:25 +0530

    revert groom father typo in seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -54,7 +54,7 @@ async function main() {
       brideParents: 'Mrs. & Mr. Mohammed Asif',
       bridePaternalGrandfather: 'Janab Haji Abdul Rasheed Saheb',
       brideMaternalGrandfather: 'Late Janab Mohammed Ismail Shariff Saheb',
-      groomFather: 'Mr. & Mr. Nawaz Ahmed',
+      groomFather: 'Mrs. & Mr. Nawaz Ahmed',
       groomPaternalGrandfather: 'Late Janab Abdul Wahab Saheb',
       groomMaternalGrandfather: 'Late Janab Syed Yusuf Saheb',
       invitationFromName: 'Mrs. & Mr. Mohammed Asif',
